66c659058fa981f75fd7c5d06a9e34ecf285dc83
unknown <joshua.aranda@SSPM-76904.SSPM.ADS>
2015-11-25 11:54:38 -0800

    Add Geolocation

---
+++ www/assets/js/app-webservice.js
@@ -0,0 +1,148 @@
+$(document).ready(function() {          
+  $('#inputSearch').keypress(function(event) {
+    if ( event.which == 13 ) {
+
+      event.preventDefault();      
+      var search = $('#inputSearch').val();
+      $(".well-card").css("display", "none");
+      $("#well-card").load(location.href+" #well-card>*","");
+      
+      $.ajax({
+        type:'GET',
+        url: "service.php?search=" + search,
+        //url: "http://joshuaranda.website/sspm/infraccion/service.php?search=" + search,
+        async: true,
+        crossDomain: true,
+        dataType: "xml",            
+        cache: false,
+        beforeSend: function(){   $(".loader").fadeOut("200").css("display", "block"); },
+        success: function(data) {
+          $(".loader").css("display", "none");
+          if(data) { $(".well-card").css("display", "block"); }
+          else { $("#textEmpty").text("No se encontró historial"); }
+        },
+        error : function(xhr, textStatus, errorThrown ) {
+          if (textStatus == 'timeout') {
+            this.tryCount++;
+            if (this.tryCount <= this.retryLimit) {
+              $.ajax(this);
+              return;
+            }            
+            return;
+          }
+          if (xhr.status == 500) {
+            console.log(xhr.responseText);
+          } else {
+            console.log(xhr.responseText);
+          }
+        }  
+      }).then(function(data) {
+
+        var xml = data;
+        
+        $.each($(xml).find('Infraccion'), function(key, value) {
+          $('.fine').append(value).append("<br/>");
+        });
+        
+        $.each($(xml).find('Fecha'), function(key, value) {
+          $('.dateFine').append(value).append("<br/>");
+        });
+        
+        $.each($(xml).find('Placa'), function(key, value) {
+          if(key < 1) {
+            $('.plate').append(value).append("<br/>");
+          }
+        });
+        
+        $.each($(xml).find('Tipo'), function(key, value) {
+          if(key < 1) {
+            $('.typeLicense').append(value).append("<br/>");
+          }
+        });
+        
+        $.each($(xml).find('Articulo'), function(key, value) {
+          $('.article').append("<br/>").append(value);
+        });
+        $.each($(xml).find('Fraccion'), function(key, value) {
+          $('.part').append(value);
+        });
+
+        $.each($(xml).find('NombresConductor'), function(key, value) {
+          if(key < 1) {
+            $('.name').append(value);
+            $('#name').append(value);
+          }
+        });
+        $.each($(xml).find('MaternoConductor'), function(key, value) {
+          if(key < 1) {
+            $('.lastName').append(value);
+            $('#lastName').append(value);
+          }
+        });
+        $.each($(xml).find('NombresConductor1'), function(key, value) {
+          if(key < 1) {
+            $('.firstName').append(value);
+            $('#firstName').append(value);
+          }
+        });
+
+        $.each($(xml).find('Licencia'), function(key, value) {
+          if(key < 1) {
+            $('.numberLicense').append(value).append("<br/>");
+          }
+        });  
+
+        $.each($(xml).find('Estado'), function(key, value) {
+          if(key < 1) {
+            $('.state').append(value).append("<br/>");
+          }
+        }); 
+
+        $.each($(xml).find('TipoPlaca'), function(key, value) {
+          if(key < 1) {
+            $('.typePlate').append(value).append("<br/>");
+          }
+        }); 
+
+        $.each($(xml).find('TipoVehiculo'), function(key, value) {
+          if(key < 1) {
+            $('.typeVehicle').append(value).append("<br/>");
+          }
+        }); 
+
+        $.each($(xml).find('Marca'), function(key, value) {
+          if(key < 1) {
+            $('.brand').append(value).append("<br/>");
+          }
+        }); 
+
+        $.each($(xml).find('Modelo'), function(key, value) {
+          if(key < 1) {
+            $('.model').append(value).append("<br/>");
+          }
+        });  
+
+        $.each($(xml).find('Serie'), function(key, value) {
+          if(key < 1) {
+            $('.serie').append(value).append("<br/>");
+          }
+        }); 
+
+        $.each($(xml).find('Color'), function(key, value) {
+          if(key < 1) {
+            $('.color').append(value).append("<br/>");
+          }
+        }); 
+
+        $.each($(xml).find('Servicio'), function(key, value) {
+          if(key < 1) {
+            $('.service').append(value).append("<br/>");
+          }
+        });           
+      });
+    }
+  });           
+  $("#other").click(function() {
+    $("#inputSearch").keypress();
+  });
+});
--- www/assets/js/wizard.js
+++ www/assets/js/wizard.js
@@ -1,43 +1,40 @@
 $(document).ready(function () {
-    //Initialize tooltips
-    $('.nav-tabs > li a[title]').tooltip();
-    
-    //Wizard
-    $('a[data-toggle="tab"]').on('show.bs.tab', function (e) {
+    var navListItems = $('div.setup-panel div a'),
+    allWells = $('.setup-content'),
+    allNextBtn = $('.nextBtn');
+    allWells.hide();
 
-        var $target = $(e.target);
-    
-        if ($target.parent().hasClass('disabled')) {
-            return false;
+    navListItems.click(function (e) {
+      e.preventDefault();
+      var $target = $($(this).attr('href')),
+      $item = $(this);
+
+      if (!$item.hasClass('disabled')) {
+        navListItems.removeClass('btn-primary').addClass('btn-default');
+        $item.addClass('btn-primary');
+        allWells.hide();
+        $target.show();
+        $target.find('input:eq(0)').focus();
+      }
+    });
+    allNextBtn.click(function(){
+      var curStep = $(this).closest(".setup-content"),
+      curStepBtn = curStep.attr("id"),
+      nextStepWizard = $('div.setup-panel div a[href="#' + curStepBtn + '"]').parent().next().children("a"),
+      curInputs = curStep.find("input[type='text'],input[type='url']"),
+      isValid = true;
+
+      $(".form-group").removeClass("has-error");
+      for(var i=0; i<curInputs.length; i++){
+        if (!curInputs[i].validity.valid){
+          isValid = false;
+          $(curInputs[i]).closest(".form-group").addClass("has-error");
         }
+      }
+      if (isValid) {
+        nextStepWizard.removeAttr('disabled').trigger('click');
+      }
     });
-    $(".next-step").click(function (e) {
 
-        var $active = $('.wizard .nav-tabs li.active');
-        $active.next().removeClass('disabled');
-        nextTab($active);
-    });
-    $(".prev-step").click(function (e) {
-
-        var $active = $('.wizard .nav-tabs li.active');
-        prevTab($active);
-    });
-});
-
-function nextTab(elem) {
-    $(elem).next().find('a[data-toggle="tab"]').click();
-}
-function prevTab(elem) {
-    $(elem).prev().find('a[data-toggle="tab"]').click();
-}
-
-/***************************/
-
-$('#radioBtn a').on('click', function(){
-    var sel = $(this).data('title');
-    var tog = $(this).data('toggle');
-    $('#'+tog).prop('value', sel);
-      
-    $('a[data-toggle="'+tog+'"]').not('[data-title="'+sel+'"]').removeClass('active').addClass('notActive');
-    $('a[data-toggle="'+tog+'"][data-title="'+sel+'"]').removeClass('notActive').addClass('active');
+    $('div.setup-panel div a.btn-primary').trigger('click');
 });
