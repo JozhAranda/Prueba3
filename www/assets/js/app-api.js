622070e5486e8b6daaca934d866789f5c4cba78c
unknown <joshua.aranda@SSPM-76904.SSPM.ADS>
2015-12-15 13:17:10 -0800

    Add RestFulAPI

--- www/assets/js/app-api.js
+++ www/assets/js/app-api.js
@@ -4,18 +4,26 @@ $(function() {
     event.preventDefault();       
     
     var dataFine = $('#formFine').serialize();
+    var my_delay = 2000;
 
     $.ajax({
       method: 'POST',
-      //url: 'http://soporte.policiatijuana.gob.mx:88/Api/Multa/',
+      url: 'http://soporte.policiatijuana.gob.mx:85/api/Infraccion',
       async: true,
       crossDomain: true,
       data: dataFine,
       cache: false,
       beforeSend: function(){ $("#submitFine").css('opacity', '0.8'); },
-      success: function(data) {           
-        /*$("body").load("preview.html").hide().fadeIn(1500).delay(6000);
-        window.location.href = "preview.html";*/
+      success: function(data) {
+        $.snackbar({
+          content: "Infracción registrada con éxito", 
+          timeout: 5000
+        });      
+        function redirect_function() {                   
+          $("body").load("home.html").hide().fadeIn(1500).delay(6000);
+          window.location.href = "home.html";
+        }
+        setTimeout(redirect_function, my_delay);  
       },
       error : function(xhr, textStatus, errorThrown ) {
         if (textStatus == 'timeout') {
@@ -43,4 +51,32 @@ $(function() {
 
     return false; 
   });
+});
+
+$(function () {
+  var queryString = new Array();
+  if (queryString.length == 0) {
+    if (window.location.search.split('?').length > 1) {
+      var params = window.location.search.split('?')[1].split('&');
+      for (var i = 0; i < params.length; i++) {
+        var key = params[i].split('=')[0];
+        var value = decodeURIComponent(params[i].split('=')[1]);
+        queryString[key] = value;
+      }
+    }
+  }
+  if (queryString["plate"] != null && queryString["fullName"] != null) {
+    $('#plateVehicle').val(queryString["plate"]);
+    $('#typeLicense').val(queryString["typeLicense"]);
+    $('#fullName').val(queryString["fullName"]);
+    $('#numberLicense').val(queryString["numberLicense"]);
+    $('#state').val(queryString["state"]);
+    $('#typePlate').val(queryString["typePlate"]);
+    $('#typeVehicle').val(queryString["typeVehicle"]);
+    $('#brandVehicle').val(queryString["brand"]);
+    $('#modelVehicle').val(queryString["model"]);
+    $('#lineVehicle').val(queryString["serie"]);
+    $('#colorVehicle').val(queryString["color"]);
+    $('#serviceVehicle').val(queryString["service"]);
+  }
 });
