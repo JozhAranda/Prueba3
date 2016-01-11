d72f3a40e08aaf694ac74b6690f8c17867a2d5ef
Joshua Aranda <aranda.moreno.joshua@gmail.com>
2016-01-11 12:24:31 -0800

    Add ASSETS2D

--- www/assets/js/home.js
+++ www/assets/js/home.js
@@ -43,7 +43,7 @@ $(document).ready(function() {
 });      
 */
 
-$.get(cordova.file.externalDataDirectory, function(data) {    
+$.get(cordova.file.applicationStorageDirectory + "files/", function(data) {    
   var html = '<div class="main-gallery gallery js-flickity">';
   $(data).each(function(key, element) {
     if(key >= 4) {
@@ -54,7 +54,7 @@ $.get(cordova.file.externalDataDirectory, function(data) {
 
         var first = abc.split('"');
         var image = first[1];
-        var dir = cordova.file.externalDataDirectory + image;
+        var dir = cordova.file.applicationStorageDirectory + "files/" + image;
         
         html += '<div class="gallery-cell" style="border: 1px solid #ccc;">';
         html += '<a class="fancybox" rel="group" href="'+ dir +'">';
